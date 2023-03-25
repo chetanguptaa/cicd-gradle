0672e79207dd2b76b1f62600612b710475cca7ee
chetan gupta <chetangupta123raj@gmail.com>
2023-03-25 10:43:47 +0530

    added src files

---
+++ src/test/java/com/chetan/webapp/WebAppTest.java
@@ -0,0 +1,11 @@
+package com.chetan.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTest {
+    @Test
+    void contextLoads() {
+    }
+}
